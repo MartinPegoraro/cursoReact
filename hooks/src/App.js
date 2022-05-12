477b2a8f072402f85d205deb267e5b701fc2f3a0
Martin Pegoraro <martinpegoraro8@gmail.com>
2022-05-12 16:22:44 -0300

    hooks

---
+++ hooks/src/App.js
@@ -0,0 +1,21 @@
+import { useRef } from "react";
+
+const App = () => {
+    const ref = useRef()
+    const inputRef = useRef()
+    const click = () => {
+        console.log(ref.current);
+    }
+    const focus = () => {
+        inputRef.current.focus()
+    }
+
+    return (
+        <div>
+            <input ref={inputRef}></input>
+            <button onClick={focus}>Focus</button>
+        </div>
+    )
+}
+
+export default App
